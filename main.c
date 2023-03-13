aa79cbf75a898644313df15ffc21523ef2002fb5
yourusername <email@youremail.com>
2023-03-13 10:09:38 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "menu.h" // INCLUDE = Text
+
+
+void main(){
+    while(1){
+        int sel = getMenuSelection();
+        if (sel == 4) break;
+    }
+}
---
+++ menu.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int getMenuSelection(){
+    while(1){
+        printf("Admin menu");
+        printf("1. Remote open door\n");
+        printf("2. List all cards in system\n");
+        printf("3. Add remove access\n");
+        printf("4. Exit\n");
+        printf("5. FAFE TEST SCAN CARD\n");
+        //TO DO input
+        int sel;
+
+   //     if GetInputInt("Ange val:", &sel)
+}
+    return 4;
+}
