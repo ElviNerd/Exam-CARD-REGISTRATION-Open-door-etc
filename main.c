2ff440ed0c72c70b1af5d45335c9bf34f2c02605
yourusername <email@youremail.com>
2023-03-31 11:17:56 +0200

    vg?

--- main.c
+++ main.c
@@ -194,221 +194,212 @@ return 0;
 */
 
 //*****************************VG FILES VERSION********************************
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <stdbool.h>
-#include <unistd.h>
 
-// Card structure
-typedef struct {
-    int cardNumber;
-    int hasAccess;
-    time_t registrationDate;
+// Card structure with necessary fields and a pointer to the next Card
+typedef struct Card {
+    int id;
+    int access;
+    char date[11];
+    int logged_in;
+    struct Card *next;
 } Card;
 
-// Node structure for the linked list
-typedef struct Node {
-    Card card;
-    struct Node *next;
-} Node;
-
-// Logged-in card list structure
-typedef struct {
-    int *cardNumbers;
-    size_t count;
-} LoggedInCardList;
-
-// Function to display the menu and get the user's choice
 int getMenuSelection() {
-    int selection;
+    int choice;
     while (1) {
-        printf("\nAdmin menu:\n");
+        printf("\nAdmin menu\n");
         printf("1. Remote open door\n");
         printf("2. List all cards in system\n");
         printf("3. Add/remove access\n");
         printf("4. Exit\n");
         printf("9. FAKE TEST SCAN CARD\n");
+
         printf("Enter your choice: ");
-        scanf("%d", &selection);
-        getchar(); // Clear the input buffer
-        if (selection >= 1 && selection <= 4 || selection == 9) {
-            return selection;
-        }
-        printf("Invalid choice. Please try again.\n");
-    }
-}
+        scanf("%d", &choice);
 
-// Function to load cards from files
-Node *loadCards() {
-    Node *head = NULL;
-    Node *current = NULL;
-    int cardNumber = 0;
-
-    while (1) {
-        char filename[64];
-        snprintf(filename, sizeof(filename), "cards/card_%d.dat", cardNumber);
-        FILE *file = fopen(filename, "rb");
-
-        if (file) {
-            Node *newNode = malloc(sizeof(Node));
-            fread(&newNode->card, sizeof(Card), 1, file);
-            newNode->next = NULL;
-            if (current) {
-                current->next = newNode;
-            } else {
-                head = newNode;
-            }
-            current = newNode;
-            fclose(file);
-            cardNumber++;
+        if (choice >= 1 && choice <= 4 || choice == 9) {
+            return choice;
         } else {
-            break;
+            printf("Invalid choice, please try again.\n");
         }
     }
-
-    return head;
 }
 
-// Function to save a card to a file
-void saveCardToFile(const Card *card) {
-    char filename[64];
-    snprintf(filename, sizeof(filename), "cards/card_%d.dat", card->cardNumber);
-    FILE *file = fopen(filename, "wb");
-
-    if (file) {
-        fwrite(card, sizeof(Card), 1, file);
-        fclose(file);
-    }
+// Function to add a card to the linked list of cards
+Card *addCard(Card *head, int id, int access) {
+    // Allocate memory for the new card and set its values
+    Card *newCard = (Card *)malloc(sizeof(Card));
+    newCard->id = id;
+    newCard->access = access;
+    time_t t = time(NULL);
+    struct tm *tm = localtime(&t);
+    strftime(newCard->date, sizeof(newCard->date), "%Y-%m-%d", tm);
+    newCard->logged_in = 0;
+    // Set the new card's next pointer to the current head and return the new card as the new head
+    newCard->next = head;
+    return newCard;
 }
 
-// Function to add a card to the linked list
-void addCard(Node **head, int cardNumber, int hasAccess) {
-    Node *newNode = malloc(sizeof(Node));
-    newNode->card.cardNumber = cardNumber;
-    newNode->card.hasAccess = hasAccess;
-    newNode->card.registrationDate = time(NULL);
-    newNode->next = *head;
-    *head = newNode;
-
-    // Save the card to a file
-    saveCardToFile(&newNode->card);
-}
-
-// Function to find a card in the linked list by card number
-Node *findCard(Node *head, int cardNumber) {
-    Node *current = head;
-    while (current) {
-        if (current->card.cardNumber == cardNumber) {
+// Function to find a card in the linked list by ID
+Card *findCard(Card *head, int id) {
+    Card *current = head;
+    // Traverse the linked list and return the card if the ID matches
+    while (current != NULL) {
+        if (current->id == id) {
             return current;
         }
         current = current->next;
     }
+    // Return NULL if the card is not found
     return NULL;
 }
 
-// Function// Function to print a card's information
-void printCardInfo(const Card *card) {
-    printf("Card number: %d\n", card->cardNumber);
-    printf("Has access: %s\n", card->hasAccess ? "Yes" : "No");
-    printf("Registration date: %s", ctime(&card->registrationDate));
+// Function to remove a card from the linked list by ID
+Card *removeCard(Card *head, int id) {
+    Card *current = head;
+    Card *prev = NULL;
+
+    // Traverse the linked list, find the card by ID, and remove it
+    while (current != NULL) {
+        if (current->id == id) {
+            // If the card is the head, update the head pointer
+            if (prev == NULL) {
+                head = current->next;
+            } else {
+                // Otherwise, update the previous card's next pointer
+                prev->next = current->next;
+            }
+            // Free the memory of the removed card
+            free(current);
+            return head;
+        }
+        // Move to the next card
+        prev = current;
+        current = current->next;
+    }
+    return head;
 }
 
-// Function to list all cards in the system
-void listAllCards(Node *head) {
-    Node *current = head;
-    while (current) {
-        printCardInfo(&current->card);
+// Function to print all cards in the linked list
+void printCards(Card *head) {
+    Card *current = head;
+    // Traverse the linked list and print each card's information
+    while (current != NULL) {
+        printf("Card ID: %d, Access: %s, Registered: %s\n", current->id, current->access ? "Yes": "No", current->date);
         current = current->next;
     }
 }
-
-// Function to free the memory of the linked list
-void freeList(Node *head) {
-    Node *current = head;
-    while (current) {
-        Node *temp = current;
+// Function to print all logged-in cards in the linked list
+void printLoggedInCards(Card *head) {
+    Card *current = head;// Traverse the linked list and print the ID of each logged-in card
+    while (current != NULL) {
+        if (current->logged_in) {
+            printf("Card ID: %d\n", current->id);
+        }
         current = current->next;
-        free(temp);
     }
 }
-
-// Function to add or remove access for a card
-void addRemoveAccess(Node *head, int cardNumber, int hasAccess) {
-    Node *cardNode = findCard(head, cardNumber);
-    if (cardNode) {
-        cardNode->card.hasAccess = hasAccess;
-        saveCardToFile(&cardNode->card);
-        printf("Card access updated.\n");
-    } else {
-        printf("Card not found.\n");
-    }
-}
-
-// Function to check if a card is logged in
-bool isLoggedIn(const LoggedInCardList *loggedInCardList, int cardNumber) {
-    for (size_t i = 0; i < loggedInCardList->count; i++) {
-        if (loggedInCardList->cardNumbers[i] == cardNumber) {
-            return true;
-        }
-    }
-    return false;
-}
-
-// Function to log in a card
-void logInCard(LoggedInCardList *loggedInCardList, int cardNumber) {
-    loggedInCardList->cardNumbers = realloc(loggedInCardList->cardNumbers, (loggedInCardList->count + 1) * sizeof(int));
-    loggedInCardList->cardNumbers[loggedInCardList->count++] = cardNumber;
-}
-
-// Function to log out a card
-void logOutCard(LoggedInCardList *loggedInCardList, int cardNumber) {
-    for (size_t i = 0; i < loggedInCardList->count; i++) {
-        if (loggedInCardList->cardNumbers[i] == cardNumber) {
-            loggedInCardList->cardNumbers[i] = loggedInCardList->cardNumbers[--loggedInCardList->count];
-            break;
-        }
-    }
-    loggedInCardList->cardNumbers = realloc(loggedInCardList->cardNumbers, loggedInCardList->count * sizeof(int));
+// Function to print the lamp status with a given status string and duration in seconds
+void printLampStatus(const char *status, int duration) {
+    printf("CURRENTLY LAMP IS: %s\n", status);
+    fflush(stdout);
+    sleep(duration);
 }
 
 int main() {
-    Node *head = loadCards();
-    LoggedInCardList loggedInCardList = {NULL, 0};
+    Card *head = NULL;    // Initialize the linked list with two cards
+    head = addCard(head, 1212, 1);
+    head = addCard(head, 1213, 1);
 
-    while (1) {
-        int selection = getMenuSelection();
-        switch (selection) {
+    int choice, subchoice, id;
+    Card *card;
+    while ((choice = getMenuSelection()) != 4) {
+        switch (choice) {
             case 1:
-                printf("Door opened remotely.\n");
+                printLampStatus("Green", 3);
                 break;
-            case 2:
-                listAllCards(head);
+            case 2:   
+                printCards(head);
                 break;
-            case 3: {
-                int cardNumber;
-                printf("Enter card number: ");
-                scanf("%d", &cardNumber);
-                getchar(); // Clear the input buffer
-                Node *cardNode = findCard(head, cardNumber);
-                if (cardNode) {
-                    cardNode->card.hasAccess = !cardNode->card.hasAccess;
-                    saveCardToFile(&cardNode->card);
-                    printf("Card access updated.\n");
-                } else {
-                    printf("Card not found.\n");
+            case 3:
+                while (1) {
+                    printf("\nCURRENTLY LAMP IS: Orange\n");
+                    printf("1. Remote open door\n");
+                    printf("2. List all logged in\n");
+                    printf("3. List all\n");
+                    printf("4. Add/remove access\n");
+                    printf("5. Back\n");
+
+                    printf("Enter your sub-choice: ");
+                    scanf("%d", &subchoice);
+
+                    if (subchoice == 1) {
+                        printLampStatus("Green", 3);
+                    } else if (subchoice == 2) {
+                        printLoggedInCards(head);
+                    } else if (subchoice == 3) {
+                        printCards(head);
+                    } else if (subchoice == 4) {
+                        printf("Enter the card ID: ");
+                        scanf("%d", &id);
+                        card = findCard(head, id);
+                        if (card == NULL) {
+                            printf("Card not found. Do you want to add a new card? (y/n): ");
+                            char addNew;
+                            scanf(" %c", &addNew);
+                            if (addNew == 'y' || addNew == 'Y') {
+                                printf("Enter access (1 for access, 0 for no access): ");
+                                int access;
+                                scanf("%d", &access);
+                                head = addCard(head, id, access);
+                                printf("Card added successfully.\n");
+                            }
+                        } else {
+                            printf("Card ID: %d, Access: %s\n", card->id, card->access ? "Yes" : "No");
+                            printf("Enter new access (1 for access, 0 for no access): ");
+                            int newAccess;
+                            scanf("%d", &newAccess);
+                            card->access = newAccess;
+                            printf("Access updated successfully.\n");
+                        }
+                    } else if (subchoice == 5) {
+                        break;
+                    } else {
+                        printf("Invalid sub-choice, please try again.\n");
+                    }
                 }
                 break;
-            }
-            case 4:
-                freeList(head);
-                free(loggedInCardList.cardNumbers);
-                return 0;
-                default:
-                printf("Invalid selection. Please try again.\n");
+            case 9:
+                printf("Please scan card to enter or x to exit.\n");
+                printf("CURRENTLY LAMP IS: Off\n");
+                while (1) {
+                    printf("Enter card ID or x to exit: ");
+                    char input[10];
+                    scanf("%s", input);
+                    if (strcmp(input, "x") == 0 || strcmp(input, "X") == 0) {
+                        break;
+                    }
+                    id = atoi(input);
+                    card = findCard(head, id);
+                    if (card != NULL && card->access) {
+                        printLampStatus("Green", 3);
+                        card->logged_in = 1;
+                    } else {
+                        printLampStatus("Red", 3);
+                    }
+                }
                 break;
-                }
-                }
-                return 0;
+        }
+    }
+
+    // Free memory
+    Card *temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    return 0;
 }
+              
